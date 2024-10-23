52a51f8f980d2475ce72617406f7bb6608adb2aa
yashwanth9494 <nyashwnth4321@gmail.com>
2024-10-23 23:18:56 +0530

    error

--- techtutor/settings.py
+++ techtutor/settings.py
@@ -26,9 +26,9 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'django-insecure-3fi5(9!bz1k-n0c_(6+b1&$y5v-7^ppf7&0t!_*mz@ub-l!n)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-vercel-domain.vercel.app', 'localhost']
 
 
 # Application definition
